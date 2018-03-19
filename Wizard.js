ed519dd10bb271d2d4708ef5c373f4812e431fff
Tom <tolas22@gmail.com>
2018-03-19 19:02:20 +0200

    wizard selectize

--- Wizard.js
+++ Wizard.js
@@ -5,15 +5,18 @@ $(document).on('pagebeforeshow', '#wizard', function () {
     getCities(renderCities);
     getCountries(renderCountries);
 });
+$(document).ready(function () {
+    $('.selectize-select').selectize;
+})
 
 function renderCities(results) {
     //this is the callBackFunc 
     results = $.parseJSON(results.d);
     $('#DynamicCitiesList').empty();
     $.each(results, function (i, row) {
-        dynamicLi = '<option> value="' + row.Id + '">' + row.Name + '</option>'
+        dynamicLi = '<option value="' + row.Id + '">' + row.Name + '</option>'
         $('#DynamicCitiesList').append(dynamicLi);
-        $('#DynamicCitiesList').listview('refresh');
+      //  $('#DynamicCitiesList').listview('refresh');
     });
 }
 
@@ -22,29 +25,29 @@ function renderCountries(results) {
     results = $.parseJSON(results.d);
     $('#DynamiCountryList').empty();
     $.each(results, function (i, row) {
-        dynamicLi = '<option> value="' + row.Id + '">' + row.Name + '</option>'
+        dynamicLi = '<option value="' + row.Id + '">' + row.Name + '</option>'
         $('#DynamiCountryList').append(dynamicLi);
-        $('#DynamiCountryList').listview('refresh');
+      //  $('#DynamiCountryList').listview('refresh');
     });
 }
 
-$(document).on('vclick', '#DynamicCitiesList li a', function () {
-    selectedCity.Name = $(this).attr('data-id');
-    $(this).closest('[data-role=listview]').prev('form').find('input').val(selectedCity.Name);
-    $(this).closest('[data-role=listview]').prev('form').find('input').attr("name", "city");
-    $(this).closest('[data-role=listview]').children().addClass('ui-screen-hidden');
+//$(document).on('vclick', '#DynamicCitiesList li a', function () {
+//    selectedCity.Name = $(this).attr('data-id');
+//    $(this).closest('[data-role=listview]').prev('form').find('input').val(selectedCity.Name);
+//    $(this).closest('[data-role=listview]').prev('form').find('input').attr("name", "city");
+//    $(this).closest('[data-role=listview]').children().addClass('ui-screen-hidden');
 
 
-});
+//});
 
-$(document).on('vclick', '#DynamiCountryList li a', function () {
-    selectedCity.Name = $(this).attr('data-id');
-    $(this).closest('[data-role=listview]').prev('form').find('input').val(selectedCity.Name);
-    $(this).closest('[data-role=listview]').prev('form').find('input').attr("name","country");
-    $(this).closest('[data-role=listview]').children().addClass('ui-screen-hidden');
+//$(document).on('vclick', '#DynamiCountryList li a', function () {
+//    selectedCity.Name = $(this).attr('data-id');
+//    $(this).closest('[data-role=listview]').prev('form').find('input').val(selectedCity.Name);
+//    $(this).closest('[data-role=listview]').prev('form').find('input').attr("name","country");
+//    $(this).closest('[data-role=listview]').children().addClass('ui-screen-hidden');
 
 
-});
+//});
 
 function insertEmp(array) {
        
