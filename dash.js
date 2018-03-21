a4ac339d3f48c5a410e85915427f2c829baee209
korikori10 <33927361+korikori10@users.noreply.github.com>
2018-03-21 18:29:51 +0200

    nothing workksssss

---
+++ dash.js
@@ -0,0 +1,35 @@
+﻿EmployeeInfo = new Object();
+$(document).on('load', function () {
+
+});
+
+$(document).ready(function () {
+getEmployees(renderEmployees);
+
+  //  getEmployeesnobusiness(renderEmployeesnobusiness);
+});
+
+
+
+function renderEmployees(results) {
+    //this is the callBackFunc 
+    //results = $.parseJSON(results.d);
+    EmployeeInfo = results.d
+
+    $(".selectize-select").selectize({
+        create: true,
+        valueField: 'Emp_Pass_id',
+        lableField: 'Fname',
+        sortField: 'text',
+        searchField: ['Emp_Pass_id', 'Fname', 'Lname'],
+        options: EmployeeInfo
+    });
+ 
+    //eresults = results;
+    //$('#DynamicEmployeesList').empty();
+    //$.each(results, function (i, row) {
+    //    dynamicLi = '<option value="' + row.Employee_pass_id + '">'+ row.Fname + " " + row.Lname + '</option>'
+    //    $('#DynamicEmployeesList').append(dynamicLi);
+        //$('#DynamicEmployeesList').listview('refresh');
+   // });
+}
